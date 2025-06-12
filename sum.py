3b8e70227cf870f3ad82c240a03a56429e276585
achitb <123086134+achitb@users.noreply.github.com>
2025-06-12 13:48:02 +0700

    Add sum function

---
+++ sum.py
@@ -0,0 +1,8 @@
+def sum_numbers(a, b):
+  return a + b
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+result = sum_numbers(num1, num2)
+print("The sum is:", result)
